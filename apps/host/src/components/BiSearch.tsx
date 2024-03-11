e40417d632d06b578b08c2a88453e9fe8a9135ee
gioboa <giorgiob.boa@gmail.com>
2024-03-11 23:39:24 +0100

    feat: add BiSearch cmp

---
+++ apps/host/src/components/BiSearch.tsx
@@ -0,0 +1,17 @@
+import { component$ } from '@builder.io/qwik';
+
+export const BiSearch = component$(() => (
+  <svg
+    stroke="currentColor"
+    fill="currentColor"
+    stroke-width="0"
+    viewBox="0 0 24 24"
+    color="#A1A2A7"
+    style="color:#A1A2A7"
+    height="22"
+    width="22"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path>
+  </svg>
+));
