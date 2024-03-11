7d575675c7fd302bc3607ea60b7e5f8e752a484a
gioboa <giorgiob.boa@gmail.com>
2024-03-11 23:41:20 +0100

    feat: add Loader cmp

---
+++ apps/host/src/components/Loader.tsx
@@ -0,0 +1,27 @@
+import { component$ } from '@builder.io/qwik';
+
+export const Loader = component$(() => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
+      viewBox="0 0 24 24"
+      class="animate-spin"
+      color="#ffffff"
+      height="25"
+      width="25"
+      xmlns="http://www.w3.org/2000/svg"
+      style="color: rgb(255, 255, 255);"
+    >
+      <circle cx="12" cy="20" r="2"></circle>
+      <circle cx="12" cy="4" r="2"></circle>
+      <circle cx="6.343" cy="17.657" r="2"></circle>
+      <circle cx="17.657" cy="6.343" r="2"></circle>
+      <circle cx="4" cy="12" r="2.001"></circle>
+      <circle cx="20" cy="12" r="2"></circle>
+      <circle cx="6.343" cy="6.344" r="2"></circle>
+      <circle cx="17.657" cy="17.658" r="2"></circle>
+    </svg>
+  );
+});
