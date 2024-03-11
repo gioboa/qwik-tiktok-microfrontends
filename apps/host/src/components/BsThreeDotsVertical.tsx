689f2a896304bc6bf2dd6d57da945886408d349e
gioboa <giorgiob.boa@gmail.com>
2024-03-11 23:38:17 +0100

    feat: add BsThreeDotsVertical cmp

---
+++ apps/host/src/components/BsThreeDotsVertical.tsx
@@ -0,0 +1,19 @@
+import { component$ } from '@builder.io/qwik';
+
+export const BsThreeDotsVertical = component$(() => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
+      viewBox="0 0 16 16"
+      color="#161724"
+      height="25"
+      width="25"
+      xmlns="http://www.w3.org/2000/svg"
+      style="color: rgb(22, 23, 36);"
+    >
+      <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"></path>
+    </svg>
+  );
+});
