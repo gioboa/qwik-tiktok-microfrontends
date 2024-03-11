e9dfd419d0ef47f477f7bf6991aa03065afaeba6
gioboa <giorgiob.boa@gmail.com>
2024-03-11 23:36:50 +0100

    feat: add AiOutlinePlus cmp

---
+++ apps/host/src/components/AiOutlinePlus.tsx
@@ -0,0 +1,20 @@
+import { component$ } from '@builder.io/qwik';
+
+export const AiOutlinePlus = component$(() => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
+      viewBox="0 0 1024 1024"
+      color="#000000"
+      style="color:#000000"
+      height="22"
+      width="22"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M482 152h60q8 0 8 8v704q0 8-8 8h-60q-8 0-8-8V160q0-8 8-8Z"></path>
+      <path d="M192 474h672q8 0 8 8v60q0 8-8 8H160q-8 0-8-8v-60q0-8 8-8Z"></path>
+    </svg>
+  );
+});
