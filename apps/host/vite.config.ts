e265c3c88f62a393d5ed430967b5c6f17cfa6217
gioboa <giorgiob.boa@gmail.com>
2024-03-13 12:07:53 +0100

    feat: add qwikReact

--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -1,4 +1,5 @@
 import { qwikCity } from '@builder.io/qwik-city/vite';
+import { qwikReact } from '@builder.io/qwik-react/vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikNxVite } from 'qwik-nx/plugins';
 import { ServerOptions, defineConfig } from 'vite';
@@ -12,6 +13,7 @@ export default defineConfig(({ mode }) => {
   return {
     cacheDir: '../../node_modules/.vite/apps/host',
     plugins: [
+      qwikReact(),
       qwikNxVite(),
       qwikCity(),
       qwikVite({
@@ -22,6 +24,7 @@ export default defineConfig(({ mode }) => {
           outDir: '../../dist/apps/host/server',
         },
       }),
+
       tsconfigPaths({ root: '../../' }),
     ],
     server: {
