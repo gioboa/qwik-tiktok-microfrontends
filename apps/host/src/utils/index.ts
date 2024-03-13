59e64a9b889d12172c86668030f74e86adc7f634
gioboa <giorgiob.boa@gmail.com>
2024-03-13 12:07:21 +0100

    chore: centralize setError method

--- apps/host/src/utils/index.ts
+++ apps/host/src/utils/index.ts
@@ -1,4 +1,6 @@
+import { Signal } from '@builder.io/qwik';
 import { JWT_COOKIE_KEY } from './constants';
+import { ShowErrorObject } from '../components/EditProfileOverlay';
 
 export const setCookie = (value: string) => {
   const date = new Date();
@@ -6,4 +8,18 @@ export const setCookie = (value: string) => {
   const expires = 'expires=' + date.toUTCString();
   document.cookie =
     JWT_COOKIE_KEY + '=' + (value || '') + '; ' + expires + '; path=/';
+};
+
+export const showError = (
+  type: string,
+  errorSig: Signal<ShowErrorObject | null>,
+) => {
+  if (
+    errorSig.value &&
+    Object.entries(errorSig.value).length > 0 &&
+    errorSig.value!.type === type
+  ) {
+    return errorSig.value.message;
+  }
+  return '';
 };
