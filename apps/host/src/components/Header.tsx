35efd5312f36fda214004465aeb3edb051bf4fd3
gioboa <giorgiob.boa@gmail.com>
2024-03-13 12:08:38 +0100

    feat: add searchProfile

--- apps/host/src/components/Header.tsx
+++ apps/host/src/components/Header.tsx
@@ -1,42 +1,39 @@
-import { component$, useContext, useSignal } from '@builder.io/qwik';
+import { component$, useContext, useSignal, useTask$ } from '@builder.io/qwik';
 import { Link, useLocation, useNavigate } from '@builder.io/qwik-city';
 import { StoreContext } from '../routes/layout';
-import { createBucketUrl } from '../utils/actions';
+import { createBucketUrl, searchProfilesByName } from '../utils/actions';
 import { LogOutIcon } from './icons/LogOutIcon';
 import { OutlinePlusIcon } from './icons/OutlinePlusIcon';
 import { SearchIcon } from './icons/SearchIcon';
 import { ThreeDotsVerticalIcon } from './icons/ThreeDotsVerticalIcon';
 import { UserIcon } from './icons/UserIcon';
 
+export type RandomUsers = {
+  id: string;
+  name: string;
+  image: string;
+};
+
 export const Header = component$(() => {
   const location = useLocation();
   const appStore = useContext(StoreContext);
   const navigate = useNavigate();
-
-  //const [searchProfiles, setSearchProfiles] = useState<RandomUsers[]>([]);
   const showMenuSig = useSignal<boolean>(false);
+  const searchProfilesSig = useSignal<RandomUsers[]>([]);
+  const searchValue = useSignal('');
 
-  // const handleSearchName = debounce(
-  //   async (event: { target: { value: string } }) => {
-  //     if (event.target.value == '') return setSearchProfiles([]);
+  useTask$(({ track, cleanup }) => {
+    track(() => searchValue.value);
 
-  //     try {
-  //       const result = await useSearchProfilesByName(event.target.value);
-  //       if (result) return setSearchProfiles(result);
-  //       setSearchProfiles([]);
-  //     } catch (error) {
-  //       console.log(error);
-  //       setSearchProfiles([]);
-  //       alert(error);
-  //     }
-  //   },
-  //   500,
-  // );
+    const debounced = setTimeout(async () => {
+      const result = await searchProfilesByName(searchValue.value);
+      if (result) {
+        searchProfilesSig.value = result;
+      }
+    }, 500);
 
-  // const goTo = () => {
-  //   if (!userContext?.user) return setIsLoginOpen(true);
-  //   router.push('/upload');
-  // };
+    cleanup(() => clearTimeout(debounced));
+  });
 
   return (
     <div class="fixed bg-white z-30 flex items-center w-full border-b h-[60px]">
@@ -47,41 +44,45 @@ export const Header = component$(() => {
       >
         <Link href="/">
           <div class="min-w-[115px] w-[115px]">
-            <img width="115" height="30" src="./tiktok-logo.png" />
+            <img width="115" height="30" src="/images/tiktok-logo.png" />
           </div>
         </Link>
 
         <div class="relative hidden md:flex items-center justify-end bg-[#F1F1F2] p-1 rounded-full max-w-[430px] w-full">
           <input
             type="text"
-            onChange$={() => {
-              console.log('handleSearchName');
-            }}
+            bind:value={searchValue}
+            onFocus$={() => (searchProfilesSig.value = [])}
             class="w-full pl-3 my-2 bg-transparent placeholder-[#838383] text-[15px] focus:outline-none"
             placeholder="Search accounts"
           />
 
-          {/* {searchProfiles.length > 0 ? (
+          {searchProfilesSig.value.length > 0 && (
             <div class="absolute bg-white max-w-[910px] h-auto w-full z-20 left-0 top-12 border p-1">
-              {searchProfiles.map((profile, index) => (
+              {searchProfilesSig.value.map((profile, index) => (
                 <div class="p-1" key={index}>
-                  <Link
-                    href={`/profile/${profile?.id}`}
+                  <button
                     class="flex items-center justify-between w-full cursor-pointer hover:bg-[#F12B56] p-1 px-2 hover:text-white"
+                    onClick$={() => {
+                      searchProfilesSig.value = [];
+                      searchValue.value = '';
+                      navigate(`/profile/${profile?.id}`);
+                    }}
                   >
                     <div class="flex items-center">
                       <img
                         class="rounded-md"
-                        width="40"
-                        src={useCreateBucketUrl(profile?.image)}
+                        height="35"
+                        width="35"
+                        src={createBucketUrl(profile?.image)}
                       />
                       <div class="truncate ml-2">{profile?.name}</div>
                     </div>
-                  </Link>
+                  </button>
                 </div>
               ))}
             </div>
-          ) : null} */}
+          )}
 
           <div class="px-3 py-1 flex items-center border-l border-l-gray-300">
             <SearchIcon />
@@ -91,7 +92,10 @@ export const Header = component$(() => {
         <div class="flex items-center gap-3 ">
           <button
             onClick$={() => {
-              console.log('goTo()');
+              if (!appStore.user) {
+                return (appStore.isLoginOpen = true);
+              }
+              navigate('/upload/');
             }}
             class="flex items-center border rounded-sm py-[6px] hover:bg-gray-100 pl-1.5"
           >
@@ -102,9 +106,7 @@ export const Header = component$(() => {
           {!appStore.user?.id ? (
             <div class="flex items-center">
               <button
-                onClick$={() => {
-                  appStore.isLoginOpen = true;
-                }}
+                onClick$={() => (appStore.isLoginOpen = true)}
                 class="flex items-center bg-[#F02C56] text-white border rounded-md px-3 py-[6px]"
               >
                 <span class="whitespace-nowrap mx-4 font-medium text-[15px]">
@@ -142,7 +144,7 @@ export const Header = component$(() => {
                     </button>
                     <button
                       onClick$={async () => {
-                        // await userContext?.logout();
+                        appStore.user = undefined;
                         showMenuSig.value = false;
                       }}
                       class="flex items-center justify-start w-full py-3 px-1.5 hover:bg-gray-100 border-t cursor-pointer"
