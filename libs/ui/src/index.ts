f3b5954358012d46fb8117ece0bdb65b9c8a4d1e
gioboa <giorgiob.boa@gmail.com>
2024-03-14 13:01:24 +0100

    chore: cleanup libs

--- libs/ui/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from '../../../apps/host/src/components/Footer';
-export * from './lib/button';
-export * from './lib/cart-counter';
-export * from './lib/header';
-export * from './lib/product';
