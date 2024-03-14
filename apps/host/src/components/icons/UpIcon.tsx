022253668c4c55a6173d98246abaeb6e30e37d96
gioboa <giorgiob.boa@gmail.com>
2024-03-14 08:38:56 +0100

    feat: add UpIcon

---
+++ apps/host/src/components/icons/UpIcon.tsx
@@ -0,0 +1,15 @@
+export const UpIcon = () => (
+  <svg
+    stroke="currentColor"
+    fill="currentColor"
+    stroke-width="0"
+    viewBox="0 0 24 24"
+    color="#FFFFFF"
+    height="30"
+    width="30"
+    xmlns="http://www.w3.org/2000/svg"
+    style="color: rgb(255, 255, 255);"
+  >
+    <path d="m6.293 13.293 1.414 1.414L12 10.414l4.293 4.293 1.414-1.414L12 7.586z"></path>
+  </svg>
+);
