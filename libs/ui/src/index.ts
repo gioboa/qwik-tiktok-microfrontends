cf61802f3cb531fefe16fa77aec1982b3d26c868
gioboa <giorgiob.boa@gmail.com>
2024-03-14 14:47:28 +0100

    chore: add constants and export UI components

---
+++ libs/ui/src/index.ts
@@ -0,0 +1,6 @@
+export * from './lib/LoaderIcon';
+export * from './lib/OutlineCheckCircleIcon';
+export * from './lib/OutlineCloseIcon';
+export * from './lib/OutlinePlusIcon';
+export * from './lib/SearchIcon';
+export * from './lib/ThreeDotsVerticalIcon';
