85df3aec15602d15ed90b56144e3651fdd2d645b
gioboa <giorgiob.boa@gmail.com>
2024-03-15 21:10:45 +0100

    chore: remove old proxy config

--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -2,10 +2,8 @@ import { qwikCity } from '@builder.io/qwik-city/vite';
 import { qwikReact } from '@builder.io/qwik-react/vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikNxVite } from 'qwik-nx/plugins';
-import { ServerOptions, defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { remotes } from '../../shared/remotes';
-import { fixRemoteHTMLInDevMode } from './shared';
 
 export default defineConfig(({ mode }) => {
   const isDev = mode !== 'production';
@@ -32,7 +30,6 @@ export default defineConfig(({ mode }) => {
         // Allow serving files from the project root
         allow: ['../../'],
       },
-      proxy: getProxy(isDev),
     },
     preview: {
       headers: {
@@ -49,42 +46,3 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
-
-const getProxy = (isDev: boolean) => {
-  let proxy: ServerOptions['proxy'] = {};
-  Object.values(remotes).forEach(({ name, url }) => {
-    proxy![`^/${name}/.*`] = {
-      target: url.replace(`${name}/`, ''),
-      changeOrigin: true,
-      selfHandleResponse: isDev,
-      rewrite: (path) =>
-        path.indexOf(`/${name}/build/`) === 0 ||
-        path.indexOf(`/${name}/src/`) === 0
-          ? path.replace(`/${name}`, '')
-          : path,
-      configure: (proxy, _options) => {
-        proxy.on('proxyRes', (proxyRes, req, res) => {
-          if (isDev) {
-            const chunks: Buffer[] = [];
-            proxyRes.on('data', (chunk) => chunks.push(chunk));
-            proxyRes.on('end', function () {
-              const decoder = new TextDecoder();
-              const rawHtml = decoder.decode(Buffer.concat(chunks));
-              if (req.url!.slice(-3) === '.js') {
-                res.setHeader('Content-Type', 'application/javascript');
-                res.write(rawHtml);
-              } else {
-                res.setHeader('Content-Type', 'text/html');
-                const fixedHtmlObj = fixRemoteHTMLInDevMode(rawHtml, '', isDev);
-                res.write(fixedHtmlObj.html);
-              }
-              res.end();
-            });
-          }
-        });
-      },
-    };
-  });
-
-  return proxy;
-};
