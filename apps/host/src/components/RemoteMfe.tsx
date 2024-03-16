ac466a9e2e751838de05cf8cfaa4a0b0889cc90d
gioboa <giorgiob.boa@gmail.com>
2024-03-16 11:25:43 +0100

    feat: add headers to RemoteMfe

--- apps/host/src/components/RemoteMfe.tsx
+++ apps/host/src/components/RemoteMfe.tsx
@@ -4,40 +4,50 @@ import {
   SSRStreamBlock,
   StreamWriter,
 } from '@builder.io/qwik';
+import { TOKEN_COOKIE_KEY } from 'shared/constants';
 import { type RemoteData } from '../../../../shared/remotes';
 import { fixRemotePathsInDevMode } from '../../shared';
 
 export interface Props {
   remote: RemoteData;
   removeLoader?: boolean;
+  token?: string;
 }
 
-export default component$(({ remote, removeLoader = false }: Props) => {
-  const url = remote.url;
-  const decoder = new TextDecoder();
-  const getSSRStreamFunction =
-    (remoteUrl: string) => async (stream: StreamWriter) => {
-      const _remoteUrl = new URL(remoteUrl);
-      if (removeLoader) {
-        _remoteUrl.searchParams.append('loader', 'false');
-      }
-      const reader = (
-        await fetch(_remoteUrl, { headers: { accept: 'text/html' } })
-      ).body!.getReader();
-      let fragmentChunk = await reader.read();
-      let base = '';
-      while (!fragmentChunk.done) {
-        const rawHtml = decoder.decode(fragmentChunk.value);
-        const fixedHtmlObj = fixRemotePathsInDevMode(rawHtml, base);
-        base = fixedHtmlObj.base;
-        stream.write(fixedHtmlObj.html);
-        fragmentChunk = await reader.read();
-      }
-    };
+export default component$(
+  ({ remote, removeLoader = false, token = '' }: Props) => {
+    console.log(token);
+    const url = remote.url;
+    const decoder = new TextDecoder();
+    const getSSRStreamFunction =
+      (remoteUrl: string) => async (stream: StreamWriter) => {
+        const _remoteUrl = new URL(remoteUrl);
+        if (removeLoader) {
+          _remoteUrl.searchParams.append('loader', 'false');
+        }
+        const reader = (
+          await fetch(_remoteUrl, {
+            headers: {
+              accept: 'text/html',
+              [TOKEN_COOKIE_KEY]: token,
+            },
+          })
+        ).body!.getReader();
+        let fragmentChunk = await reader.read();
+        let base = '';
+        while (!fragmentChunk.done) {
+          const rawHtml = decoder.decode(fragmentChunk.value);
+          const fixedHtmlObj = fixRemotePathsInDevMode(rawHtml, base);
+          base = fixedHtmlObj.base;
+          stream.write(fixedHtmlObj.html);
+          fragmentChunk = await reader.read();
+        }
+      };
 
-  return (
-    <SSRStreamBlock>
-      <SSRStream>{getSSRStreamFunction(url)}</SSRStream>
-    </SSRStreamBlock>
-  );
-});
+    return (
+      <SSRStreamBlock>
+        <SSRStream>{getSSRStreamFunction(url)}</SSRStream>
+      </SSRStreamBlock>
+    );
+  },
+);
