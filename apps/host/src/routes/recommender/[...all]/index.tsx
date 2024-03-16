fa78192fe1a49d011506857fbc86e2800d2e14b5
gioboa <giorgiob.boa@gmail.com>
2024-03-16 11:26:21 +0100

    feat: add recommender path

---
+++ apps/host/src/routes/recommender/[...all]/index.tsx
@@ -0,0 +1,57 @@
+import { RequestHandler } from '@builder.io/qwik-city';
+import { TOKEN_COOKIE_KEY } from 'shared/constants';
+import { remotes } from 'shared/remotes';
+import { fixRemotePathsInDevMode } from '../../../../shared';
+
+export const onRequest: RequestHandler = async ({
+  send,
+  url,
+  cookie,
+  redirect,
+  request,
+}) => {
+  if (!cookie.get(TOKEN_COOKIE_KEY)?.value) {
+    throw redirect(307, '/');
+  }
+
+  const remoteName = 'recommender';
+  const pathName = url.pathname.replace(`/${remoteName}/`, '');
+  const remoteUrl = pathName
+    ? remotes[remoteName].url.replace(`${remoteName}/`, '')
+    : remotes[remoteName].url;
+
+  let response = await fetch(remoteUrl + pathName + url.search, {
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type':
+        request.headers.get('Content-Type') || 'application/javascript',
+      'Access-Control-Allow-Origin': '*',
+      [TOKEN_COOKIE_KEY]: cookie.get(TOKEN_COOKIE_KEY)?.value || '',
+    },
+  });
+
+  if (needNewResponse(remoteUrl + pathName)) {
+    let contentType = 'text/html';
+    if (isJSorTS(pathName)) {
+      contentType = 'application/javascript';
+    }
+
+    let content = await response.text();
+    content = fixRemotePathsInDevMode(content, remoteName).html;
+
+    response = new Response(content, {
+      status: 200,
+      headers: { 'Content-Type': contentType },
+    });
+  }
+
+  send(response);
+};
+
+const needNewResponse = (url: string) => isJSorTS(url) || isHtml(url);
+
+const isJSorTS = (url: string) =>
+  ['.js', '.ts'].includes(url.slice(-3)) ||
+  ['.jsx', '.tsx'].includes(url.slice(-4));
+
+const isHtml = (url: string) => url.slice(-1) === '/';
